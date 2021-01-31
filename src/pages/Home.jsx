125e2764d807171810c75361d2688ef10cf62f2a
nana-boateng <nana.boateng@queensu.ca>
2021-01-31 01:51:14 -0500

    Added dummy recipes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,29 +4,49 @@ import Fridge from "../components/Fridge";
 import Card from "./../components/ui/Card";
 
 export default function Home() {
+  const recipes = [
+    {
+      key: 1,
+      name: "Glass of Milk",
+      desc: "Ya only need cookies!",
+      ingredients: ["Milk"],
+    },
+    {
+      key: 2,
+      name: "Scrambled Eggs",
+      desc: "Jus' need some oil!",
+      ingredients: ["Eggs"],
+    },
+    {
+      key: 3,
+      name: "Toast",
+      desc: "Jus' need some butter!",
+      ingredients: ["Bread", "Toaster"],
+    },
+  ];
+
+  function getCards() {
+    const allRecipes = [];
+
+    for (let i = 0; i < recipes.length; i++) {
+      allRecipes.push(
+        <Card
+          recipeName={recipes[i].name}
+          desc={recipes[i].desc}
+          ingredients={recipes[i].ingredients}
+        />
+      );
+    }
+    return allRecipes;
+  }
+
   return (
     <div>
       <NavBar />
       <div>
         <Fridge />
       </div>
-      <div className="flex flex-row m-8">
-        <Card
-          recipeName="Glass of milk"
-          desc="A meal so filling, its all Santa drinks!"
-          ingredients={["Milk"]}
-        />
-        <Card
-          recipeName="Scrambled Eggs"
-          desc="Jus' need some butter!"
-          ingredients={["Eggs"]}
-        />
-        <Card
-          recipeName="Toast"
-          desc="Jus' need some butter!"
-          ingredients={["Bread", "Toaster"]}
-        />
-      </div>
+      <div className="flex flex-row m-8">{getCards()}</div>
     </div>
   );
 }
