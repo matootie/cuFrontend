eb8a94480d8339cd83c6d3fe8aa70f29966bb54b
nana-boateng <nana.boateng@queensu.ca>
2021-01-31 02:13:57 -0500

    Fixed formatting

--- src/components/Fridge.jsx
+++ src/components/Fridge.jsx
@@ -1,36 +1,28 @@
-import React, { useContext} from "react";
+import React, { useContext } from "react";
 import AddFridgeItem from "./Modal/AddFridgeItem";
 //import EditFridgeItem from "./Modal/EditFridgeItem";
 
 import { UserContext } from "./../util/UserContext";
 
-
 export default function Fridge() {
-
   const { recipe, fridge } = useContext(UserContext);
   const [userFridge, setUserFridge] = fridge;
-  
-  function getFridgeItem(){
-    return userFridge.forEach(element => {
+
+  function getFridgeItem() {
+    return userFridge.forEach((element) => {
       <span className="p-5 m-1 bg-gray-100 rounded-lg" key={element}>
         {element}
-      </span>
+      </span>;
     });
-  };
+  }
 
-  return(
-  <div className="flex flex-col justify-between">
-    <div className="flex flex-row flex-wrap ">
-      {getFridgeItem()}
+  return (
+    <div className="flex flex-col justify-between">
+      <div className="flex flex-row flex-wrap ">{getFridgeItem()}</div>
+      <div className="flex flex-row">
+        <AddFridgeItem />
+      </div>
+      <div>{/* <EditFridgeItem/ > */}</div>
     </div>
-    <div className="flex flex-row" id="fuck-tits">
-      <AddFridgeItem />
-    </div>
-    <div>
-      {/* <EditFridgeItem/ > */}
-    </div>
-  </div>
   );
 }
-
-
