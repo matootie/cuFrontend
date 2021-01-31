3247c308758444cc52145200a1c6c58cd4c6422d
nana-boateng <nana.boateng@queensu.ca>
2021-01-31 01:40:29 -0500

    Change class to className

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,47 +3,47 @@ import React from "react";
 
 export default function NavBar() {
   return (
-    <nav class="bg-gray-800">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="flex items-center justify-between h-16">
-          <div class="flex items-center">
-            <div class="flex-shrink-0">
-              <img class="h-8 w-8" src="/logo.png" alt="Workflow" />
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0">
+              <img className="h-8 w-8" src="/logo.png" alt="Workflow" />
             </div>
-            <div class="hidden md:block">
-              <div class="ml-10 flex items-baseline space-x-4">
+            <div className="hidden md:block">
+              <div className="ml-10 flex items-baseline space-x-4">
                 {/* <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" --> */}
                 <a
                   href="#"
-                  class="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Dashboard
                 </a>
 
                 <a
                   href="#"
-                  class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                 >
                   Recipes
                 </a>
               </div>
             </div>
           </div>
-          <div class="hidden md:block">
-            <div class="ml-4 flex items-center md:ml-6">
+          <div className="hidden md:block">
+            <div className="ml-4 flex items-center md:ml-6">
               {/* <!-- Profile dropdown --> */}
-              <div class="ml-3 relative">
+              <div className="ml-3 relative">
                 <div>
                   <button
-                    class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                    className="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                     id="user-menu"
                     aria-haspopup="true"
                   >
-                    <span class="sr-only">Open user menu</span>
+                    <span className="sr-only">Open user menu</span>
                     <img
-                      class="h-8 w-8 rounded-full"
+                      className="h-8 w-8 rounded-full"
                       src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                      alt=""
+                      alt="Profile Picture"
                     />
                   </button>
                 </div>
